20232c6065030ebaa8724506c9248c6237a0cb42
Joe-0009 <youssefrachidi1994@gmail.com>
2025-01-07 19:01:31 +0100

    tryiing to change libmlx to try if i stil have leaks

--- fdf_events.c
+++ fdf_events.c
@@ -6,12 +6,20 @@
 /*   By: yrachidi <yrachidi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 14:14:08 by yrachidi          #+#    #+#             */
-/*   Updated: 2025/01/07 18:02:02 by yrachidi         ###   ########.fr       */
+/*   Updated: 2025/01/07 18:47:25 by yrachidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+int	close_window_x(t_vars *vars)
+{
+	cleanup_image(vars);
+	cleanup_window(vars);
+	free_points(vars->dim.height, vars->points);
+	exit(0);
+	return (0);
+}
 int	close_window_esc(int keycode, t_vars *vars)
 {
 	if (keycode == 65307)
@@ -24,14 +32,7 @@ int	close_window_esc(int keycode, t_vars *vars)
 	return (0);
 }
 
-int	close_window_x(t_vars *vars)
-{
-	cleanup_image(vars);
-	cleanup_window(vars);
-	free_points(vars->dim.height, vars->points);
-	exit(0);
-	return (0);
-}
+
 
 void	mlx_hooks(t_vars *vars, char *str)
 {
