a901a2f5414cdf8b96f70297867e252a79bd0c69
Joe-0009 <youssefrachidi1994@gmail.com>
2025-01-14 23:11:57 +0100

    gg

--- fdf_transform.c
+++ fdf_transform.c
@@ -6,7 +6,7 @@
 /*   By: yrachidi <yrachidi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 14:15:41 by yrachidi          #+#    #+#             */
-/*   Updated: 2025/01/14 22:34:14 by yrachidi         ###   ########.fr       */
+/*   Updated: 2025/01/14 23:09:55 by yrachidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,43 +48,39 @@ void	move_map(t_point **points, t_map *map)
 	t_bounds	bounds;
 
 	find_map_boundaries(points, map, &bounds);
-	map->center.x = (WIDTH - (bounds.max_x - bounds.min_x)) / 2 ;
-	map->center.y = (HEIGHT - (bounds.max_y - bounds.min_y)) / 2 ;
+	map->center.x = (WIDTH - (bounds.max_x - bounds.min_x)) / 2;
+	map->center.y = (HEIGHT - (bounds.max_y - bounds.min_y)) / 2;
 	i = 0;
 	while (i < map->dim.height)
 	{
 		j = 0;
 		while (j < map->dim.width)
 		{
-			points[i][j].x += (map->center.x - bounds.min_x + map->center.offset_x);
-			points[i][j].y += (map->center.y - bounds.min_y + map->center.offset_y);
+			points[i][j].x += (map->center.x - bounds.min_x
+					+ map->center.offset_x);
+			points[i][j].y += (map->center.y - bounds.min_y
+					+ map->center.offset_y);
 			j++;
 		}
 		i++;
 	}
 }
 
-
-void    update_zoom(t_vars *vars, float zoom_delta)
+void	update_zoom(t_vars *vars, float zoom_delta)
 {
-    vars->map->scale.zoom_factor *= zoom_delta;
-    
-    if (vars->map->scale.zoom_factor < 0.1)
-        vars->map->scale.zoom_factor = 0.1;
-    if (vars->map->scale.zoom_factor > 10.0)
-        vars->map->scale.zoom_factor = 10.0;
-    calculate_scale(vars->map);
-    parse_map(vars);
-    iso_point(vars);
-    move_map(vars->points, vars->map);
+	vars->map->scale.zoom_factor *= zoom_delta;
+	if (vars->map->scale.zoom_factor < 0.1)
+		vars->map->scale.zoom_factor = 0.1;
+	if (vars->map->scale.zoom_factor > 10.0)
+		vars->map->scale.zoom_factor = 10.0;
+	calculate_scale(vars->map);
+	parse_map(vars);
+	iso_point(vars);
+	move_map(vars->points, vars->map);
 }
 
 void	iso_point(t_vars *vars)
 {
-		apply_rotation(vars, 0.785398, 'z');
-		apply_rotation(vars, 0.95, 'x');
-		
+	apply_rotation(vars, 0.785398, 'z');
+	apply_rotation(vars, 0.95, 'x');
 }
-
-
-
